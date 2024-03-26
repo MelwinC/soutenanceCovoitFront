d4c883fc23c54cc1dbef68f6dbd750ab2c3d7c88
MelwinC <contact@melwinchenu.fr>
2024-03-26 01:39:21 +0100

    [package] add datepicker component

---
+++ src/components/DatePicker.tsx
@@ -0,0 +1,51 @@
+import { format } from "date-fns";
+import { fr } from "date-fns/locale";
+import { Calendar as CalendarIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
+
+const DatePicker = ({
+  date,
+  setDate,
+}: {
+  date: Date | undefined;
+  setDate: React.Dispatch<React.SetStateAction<Date | undefined>>;
+}) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"navbarLogo"}
+          className={cn(
+            "w-full justify-start text-left font-normal bg-white py-6 text-slate-700",
+            !date && "text-muted-foreground"
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4 text-slate-700" />
+          {date ? (
+            format(date, "PPP", { locale: fr })
+          ) : (
+            <span className="text-slate-700">Choisissez un jour</span>
+          )}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default DatePicker;
