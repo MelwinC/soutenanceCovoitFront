50a68c247fc7997921d6f7b49ad6398c2c787063
MelwinC <contact@melwinchenu.fr>
2024-03-27 21:16:22 +0100

    [fix] refacto Toast - WIP replace

---
+++ src/components/Toast.tsx
@@ -0,0 +1,22 @@
+import { capitalize } from "@/lib/utils";
+import { CheckCircle, CircleX } from "lucide-react";
+import { toast } from "./ui/use-toast";
+
+const Toast = (success: boolean, description: string) => {
+  toast({
+    description: (
+      <span className="flex items-center">
+        {success ? (
+          <CheckCircle style={{ color: "green" }} />
+        ) : (
+          <CircleX style={{ color: "red" }} />
+        )}
+        <p className="pl-4 text-[1rem]">{capitalize(description)}</p>
+      </span>
+    ),
+    duration: 4000,
+    variant: "success",
+  });
+};
+
+export default Toast;
